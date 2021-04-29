32d519797758845e3923174a26b7465c4aad0406
bailram <>
2021-04-29 12:04:34 +0700

    task1

---
+++ src/main/java/com/infosys/learning/dto/Person.java
@@ -0,0 +1,16 @@
+package com.infosys.learning.dto;
+
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
