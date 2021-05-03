e90eda1c36297a11ae6da6949e7b5125bea0c286
bailram <>
2021-05-03 11:33:53 +0700

    update getPerson method. Forget to set name person with value of name from parameter

--- src/main/java/com/infosys/learning/service/LearningService.java
+++ src/main/java/com/infosys/learning/service/LearningService.java
@@ -51,7 +51,7 @@ public class LearningService {
         Data data = new Data();
         int dateNow = LocalDate.now().getYear();
 
-        person.setName("John");
+        person.setName(name);
         data.setAge(dateNow-yearOfBirth);
         person.setData(data);
 
